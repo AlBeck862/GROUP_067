e52cac3a16c6f4a835988567dc3d1ba975a2ea59
Arthur Boschet <arthur.bleu25@gmail.com>
2022-04-20 13:03:37 -0400

    added layer to both the critic and actor

--- GROUP_067/models/td3.py
+++ GROUP_067/models/td3.py
@@ -28,6 +28,7 @@ class Actor(nn.Module):
 
         self.l1 = nn.Linear(state_dim, 400)
         self.l2 = nn.Linear(400, 300)
+        self.l2_ = nn.Linear(400, 300)
         self.l3 = nn.Linear(300, action_dim)
 
         self.max_action = max_action
@@ -35,6 +36,7 @@ class Actor(nn.Module):
     def forward(self, x):
         x = F.relu(self.l1(x))
         x = F.relu(self.l2(x))
+        x = F.relu(self.l2_(x))
         x = self.max_action * torch.tanh(self.l3(x)) 
         return x
 
@@ -59,11 +61,13 @@ class Critic(nn.Module):
         # Q1 architecture
         self.l1 = nn.Linear(state_dim + action_dim, 400)
         self.l2 = nn.Linear(400, 300)
+        self.l2_ = nn.Linear(400, 300)
         self.l3 = nn.Linear(300, 1)
 
         # Q2 architecture
         self.l4 = nn.Linear(state_dim + action_dim, 400)
         self.l5 = nn.Linear(400, 300)
+        self.l5_ = nn.Linear(400, 300)
         self.l6 = nn.Linear(300, 1)
 
     def forward(self, x, u):
@@ -71,10 +75,12 @@ class Critic(nn.Module):
 
         x1 = F.relu(self.l1(xu))
         x1 = F.relu(self.l2(x1))
+        x1 = F.relu(self.l2_(x1))
         x1 = self.l3(x1)
 
         x2 = F.relu(self.l4(xu))
         x2 = F.relu(self.l5(x2))
+        x2 = F.relu(self.l5_(x2))
         x2 = self.l6(x2)
         return x1, x2
 
